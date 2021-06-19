1536eba38f388cc248c5f79c5d6d507ed675e369
Ridjy <r.ridjy@gmail.com>
2021-06-19 19:47:57 +0300

    menu et dashboard OK

--- src/Controller/DocumentController.php
+++ src/Controller/DocumentController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Users;
 
 class DocumentController extends AbstractController
 {
@@ -13,8 +14,10 @@ class DocumentController extends AbstractController
      */
     public function index(): Response
     {
+        $o_allUsers = $this->getDoctrine()->getRepository(Users::class)->findAll();    
         return $this->render('document/index.html.twig', [
-            'controller_name' => 'DocumentController',
+            'alluser'  => $o_allUsers,
+            'nav' => 'dashboard'
         ]);
     }
 }
