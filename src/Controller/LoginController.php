ecc5f6ae106ab9c9f559488c6481910f76af52ab
Ridjy <r.ridjy@gmail.com>
2021-06-19 16:55:13 +0300

    connexion test user OK

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Entity\Users;
 
 class LoginController extends AbstractController
 {
@@ -21,22 +25,30 @@ class LoginController extends AbstractController
     /**
      * @Route("/connexion", name="connexion")
      */
-    public function connexion()
+    public function connexion(Request $Request, SessionInterface $session)
     {
         // les parametres envoyés par le datatable
         if ($Request->getMethod() == 'POST') {
-            $login = $Request->request->get('login');
-            $mdp = $Request->request->get('mdp');
+            $s_email = $Request->request->get('login');
+            $s_mdp = $Request->request->get('mdp');
         } else // si la requete POST n'est pas lancé, on n'a pas besoin de ce script
             die;
 
-        $entityManager = $this->getDoctrine()->getManager();
-        
-        $response = $this->forward('App\Controller\LoginController::index', [
-            'login'  => $name,
-            'mdp' => 'green',
-        ]);
-        
+        //$entityManager = $this->getDoctrine()->getManager();
+        $o_users = $this->getDoctrine()->getRepository(Users::class)->findOneBy(array('email' => $s_email,'password' => md5($s_mdp)));
+        if(!$o_users)
+        {
+            $response = $this->forward('App\Controller\LoginController::index', [
+                'login'  => $s_email,
+                'mdp' => $s_mdp,
+            ]);
+        } else {
+            //sessions
+            $session->set('user_connecte', $o_users);
+            $response = $this->forward('App\Controller\DocumentController::index', [
+                'login'  => $s_email
+            ]);
+        }    
         return $response;
         
     }//end connexion
